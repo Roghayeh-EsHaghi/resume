339c9f7ac04c7b637f402c3e277d5abf52cd1e9c
roghayeh eshaghi <r.eshaghi226@gmail.com>
2021-12-31 14:27:10 +0330

    home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./assets/styles/App.css";
 
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./assets/styles/App.css";
+import Homepage from "./components/homepage";
 function App() {
-  return <div></div>;
+  const routes = [{ path: "/", component: Homepage }];
+  return (
+    <Router basename={window.location.pathname || ""}>
+      <Switch>
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            exact
+            component={route.component}
+            path={route.path}
+          />
+        ))}
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/homepage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Homepage(props) {
+  return <div>{/* <p>رقیه اسحاقی</p> */}</div>;
+}
